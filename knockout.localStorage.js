d8f9c21f6abf338edccf8fd8b308d3e5f1cbcaa6
sketchytechky <boris.w.lau@gmail.com>
2013-02-27 02:53:23 -0800

    added poll based notification for notifying local storage changes

--- knockout.localStorage.js
+++ knockout.localStorage.js
@@ -1,6 +1,12 @@
 (function(ko){
+  "use strict";
+
+  var DEFAULT_POLL_FREQUENCY = 1000;
+
   // Wrap ko.observable and ko.observableArray
   var methods = ['observable', 'observableArray'];
+
+  var lastValue = {};
 
   ko.utils.arrayForEach(methods, function(method){
     var saved = ko[method];
@@ -19,12 +25,32 @@
 
       // Create observable from saved method
       var observable = saved(initialValue);
+      if (options.synclocal) lastValue[key] = initialValue;
 
       // Subscribe to changes, and save to localStorage
       if(key){
         observable.subscribe(function(newValue){
           localStorage.setItem(key, ko.toJSON(newValue));
+          if (options.synclocal) lastValue[key] = newValue;
         });
+
+        // Be notify of the changes at the localStorage
+        if (options.synclocal) {
+          (function (saved, key) {
+            setInterval(function () {
+              try{
+                var localVal = JSON.parse(localStorage.getItem(key))
+                if (lastValue[key] != localVal) {
+                  observable(localVal);
+                  lastValue[key] = localVal;
+                }
+              }catch(e){
+                console.warn(e);
+              };
+            }, options.pollfreq || DEFAULT_POLL_FREQUENCY);
+          })(saved, key);
+        }
+
       };
 
       return observable;
